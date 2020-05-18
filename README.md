8a42ba566324dde7e99064d88824ae055d305594
Patrick <patrick204nqh@gmail.com>
2020-05-18 13:52:43 +0700

    data & methods

---
+++ README.md
@@ -0,0 +1,1 @@
+# Patrick's Project( using Vue.js)
--- app.js
+++ app.js
@@ -2,5 +2,11 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Patrick',
+    job: 'Developer',
+  },
+  methods: {
+    greet: function (time) {
+      return 'Good ' + time + ' ' + this.name;
+    },
   },
 });
