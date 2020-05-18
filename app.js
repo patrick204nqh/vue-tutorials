2490611379db4d7955c86199a14a1e3f52e3bd15
Patrick <patrick204nqh@gmail.com>
2020-05-18 14:26:41 +0700

    events --important

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'Patrick',
-    job: 'Developer',
-    website: 'https://github.com/patrick-nqh-bl/vue-playlist',
-    websiteTag:
-      '<a href="https://github.com/patrick-nqh-bl/vue-playlist">Github Repo</a>',
+    age: 25,
+    x: 0,
+    y: 0,
   },
   methods: {
-    greet: function (time) {
-      return 'Good ' + time + ' ' + this.name;
+    add: function (inc) {
+      this.age += inc;
+    },
+    subtract: function (dec) {
+      this.age -= dec;
+    },
+    updateXY: function (e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     },
   },
 });
