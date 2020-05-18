959a7500cc2baa7a2989827271a5d4cc32f2130e
Patrick <patrick204nqh@gmail.com>
2020-05-18 14:02:28 +0700

    data binding

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ new Vue({
   data: {
     name: 'Patrick',
     job: 'Developer',
+    website: 'https://github.com/patrick-nqh-bl/vue-playlist',
+    websiteTag:
+      '<a href="https://github.com/patrick-nqh-bl/vue-playlist">Github Repo</a>',
   },
   methods: {
     greet: function (time) {
