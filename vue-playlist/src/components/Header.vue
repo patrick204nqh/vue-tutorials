9c802a2c9ced2698742caeb269a753cb83256ae9
Patrick <patrick204nqh@gmail.com>
2020-05-19 23:58:40 +0700

    th event bus

--- vue-playlist/src/components/Header.vue
+++ vue-playlist/src/components/Header.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import { bus } from "../main";
 export default {
   props: {
     title: {
@@ -16,7 +17,9 @@ export default {
   },
   methods: {
     changeTitle: function() {
-      this.$emit("changeTitle", "Vue Wizards");
+      // this.$emit("changeTitle", "Vue Wizards");
+      this.title = "Vue Wizards";
+      bus.$emit("titleChanged", "Vue Wizards");
     }
   }
 };
