46cbafddb4728c8cb58e33a0cf8cee564550ca61
Patrick <patrick204nqh@gmail.com>
2020-05-19 09:19:02 +0700

    multiple vue instances

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-new Vue({
-  el: '#vue-app',
+var one = new Vue({
+  el: '#vue-app-one',
   data: {
-    health: 100,
-    ended: false,
+    title: 'Vue App One',
+  },
+  methods: {},
+  computed: {
+    greet: function () {
+      return 'Hello from app one';
+    },
+  },
+});
+
+var two = new Vue({
+  el: '#vue-app-two',
+  data: {
+    title: 'Vue App Two',
   },
   methods: {
-    punch: function () {
-      this.health -= 10;
-      if (this.health <= 0) {
-        this.ended = true;
-      }
-    },
-    restart: function () {
-      this.health = 100;
-      this.ended = false;
+    changeTitle: function () {
+      one.title = 'Title changed';
     },
   },
-  computed: {},
+  computed: {
+    greet: function () {
+      return 'App two speaking';
+    },
+  },
 });
+
+two.title = 'Changed from outside';
