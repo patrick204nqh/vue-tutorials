2a496e064fd135053205c22d2f8ca3cd24d29f60
Patrick <patrick204nqh@gmail.com>
2020-05-19 10:09:35 +0700

    Vue CLI

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 1. Source video tutorials:
 
 > https://www.youtube.com/watch?v=9Hcswbo0nCY&list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa&index=13
+
+2. Setup Vue environment:
+
+- Install vue-cli:
+
+> sudo npm i -g vue-cli
--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 new Vue({
   el: '#vue-app',
-  data: {
-    output: 'Your fav food',
-  },
-  methods: {
-    readRefs: function () {
-      console.log(this.$refs.test.innerText);
-      this.output = this.$refs.input.value;
-    },
-  },
+  data: {},
+  methods: {},
 });
